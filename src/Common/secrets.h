f5b779c361340b7e2aa4e84a3fb7e3090d701843
aocLEL <75206529+aocLEL@users.noreply.github.com>
2024-05-10 10:59:23 +0200

    Adjustments

--- src/Common/secrets.h
+++ src/Common/secrets.h
@@ -1,11 +1,7 @@
 #ifndef __SECRETS__
     #define __SECRETS__
-    const char *wifi_ssid = "Vodafone-A47766340";
-    const char *wifi_password = "X79FhpC29ExqPfh4";
-    /*
-    const char *wifi_ssid = "---SSID---";
-    const char *wifi_password = "---PASSWORD---";
-    */
+    const char *wifi_ssid = "--SSID--";
+    const char *wifi_password = "--PASSWORD--";
 #endif
 
 
