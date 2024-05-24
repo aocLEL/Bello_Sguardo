21238ccae3e3a354c286191650ec872b71b2e6fc
aocLEL <antonionotari68@gmail.com>
2024-05-24 14:30:36 +0200

    Changes

--- src/TgBot/.venv/lib/python3.12/site-packages/pyromod/config.py
+++
@@ -1,9 +0,0 @@
-from types import SimpleNamespace
-
-config = SimpleNamespace(
-    timeout_handler=None,
-    stopped_handler=None,
-    throw_exceptions=True,
-    unallowed_click_alert=True,
-    unallowed_click_alert_text=("[pyromod] You're not expected to click this button."),
-)
